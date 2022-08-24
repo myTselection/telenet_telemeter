a73fa0befdabbd4f5e85fe9b74101b3215961c94
myTselection <myTselection@gmail.com>
2022-08-24 18:33:18 +0200

    copy of strings

---
+++ custom_components/telenet_telemeter/translations/en.py
@@ -0,0 +1,48 @@
+{
+    "config": {
+        "title": "Telenet Telemeter",
+        "step": {
+            "user": {
+                "title": "Telenet Telemeter",
+                "description": "Setup a Telenet Telementer sensor, username and password are required.",
+                "data": {
+                    "username": "Username",
+                    "password": "Password"
+                }
+            },
+            "edit": {
+                "title": "Telenet Telemeter",
+                "description": "Edit Setup a Telenet Telemeter sensor, username and password are required.",
+                "data": {
+                    "username": "Username",
+                    "password": "Password"
+                }
+            }
+
+        },
+        "error": {
+            "missing username": "Please provide a valid Telenet username",
+            "missing password": "Please provide a valid Telenet password",
+            "missing data options handler": "Option handler failed",
+            "no_valid_settings": "No valid settings, provide username & password in ha config."
+        }
+    },
+    "options": {
+        "step": {
+            "edit": {
+                "title": "Telenet Telemeter",
+                "description": "Edit setup a Telenet Telemeter sensor, username and password are required.",
+                "data": {
+                    "username": "Username",
+                    "password": "Password"
+                }
+            }
+        },
+        "error": {
+            "missing username": "Please provide a valid Telenet username",
+            "missing password": "Please provide a valid Telenet password",
+            "missing data options handler": "Option handler failed",
+            "no_valid_settings": "No valid settings, provide username & password in ha config."
+        }
+    }
+}
