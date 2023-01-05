12b0a370cf1904e1d56b251d21bf576b741cab0e
myTselection <myTselection@gmail.com>
2023-01-05 22:16:54 +0100

    execution telemeter function

--- custom_components/telenet_telemeter/sensor.py
+++ custom_components/telenet_telemeter/sensor.py
@@ -77,11 +77,11 @@ class ComponentData:
     def __init__(self, username, password, client, hass):
         self._username = username
         self._password = password
-        self.client = client
+        self._client = client
         self._data = {}
         self._last_update = None
         self._friendly_name = None
-        self._session = TelenetSession(client)
+        self._session = TelenetSession(self._client)
         self._telemeter = None
         self._hass = hass
         
@@ -89,7 +89,7 @@ class ComponentData:
     async def _update(self):
         _LOGGER.debug("Fetching stuff for " + NAME)
         if not(self._session):
-            self._session = TelenetSession(client)
+            self._session = TelenetSession(self._client)
 
         if self._session:
             self._session.login(self._username, self._password, self._hass)
@@ -99,7 +99,7 @@ class ComponentData:
 
     async def update(self):
         await self._update()
-        return self._telemeter
+        return self._telemeter(self._hass)
 
 
 
@@ -113,7 +113,7 @@ class Component(Entity):
         """Return the state of the sensor."""
         #FIXME integrate Telenet telemeter data request
         # return asyncio.run_coroutine_threadsafe(self._data._telemeter, self._hass.loop).result()
-        return self._data._telemeter
+        return self._data._telemeter(self._hass)
 
     async def async_update(self):
         await self._data.update()
@@ -146,7 +146,7 @@ class Component(Entity):
             # "last update": self._data._telemeter.internetusage[0].lastupdated,
             # "peak_usage": self._data._telemeter.usages[0].totalusage.peak/1024/1024,
             # "offpeak_usage": self._data._telemeter.usages[0].totalusage.offpeak/1024/1024,
-            # "telemeter_json": self._data._telemeter
+            "telemeter_json": self._data._telemeter(self._hass)
         }
 
     @property
