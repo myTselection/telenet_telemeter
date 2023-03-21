a53187e4bd0fa73873ec0c49b7231a3c27dc5bd7
Geert Meersman <geertmeersman@gmail.com>
2023-03-21 21:17:15 +0100

    Refactoring utils | references #21

--- custom_components/telenet_telemeter/utils.py
+++ custom_components/telenet_telemeter/utils.py
@@ -42,182 +42,78 @@ def check_settings(config, hass):
 
 class TelenetSession(object):
     def __init__(self):
-        # self.s = client
         self.s = requests.Session()
         self.s.headers["User-Agent"] = "TelemeterPython/3"
+        self.s.headers["x-alt-referer"] = "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice"
+
+    def callTelenet(self, url, caller = "Not set", data = None, expectedStatusCode = "200", printResponse = False):
+        if data == None:
+            _LOGGER.debug(f"[{caller}] Calling GET {url}")
+            response = self.s.get(url,timeout=10)
+        else:
+            _LOGGER.debug(f"[{caller}] Calling POST {url}")
+            response = self.s.post(url,data,timeout=10)
+        _LOGGER.debug(f"[{caller}] http status code = {response.status_code} (expecting {expectedStatusCode})")
+        if printResponse:
+            _LOGGER.debug(f"[{caller}] Response:\n{response.text}")
+        if expectedStatusCode != None:
+            assert response.status_code == expectedStatusCode
+        
+        return response
 
     def login(self, username, password):
-        # Get OAuth2 state / nonce
-        headers = {"x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice"}
-
-        response = self.s.get("https://api.prd.telenet.be/ocapi/oauth/userdetails", headers=headers,timeout=10)
-        _LOGGER.debug("userdetails result status code:" + str(response.status_code))
-        if (response.status_code == 200):
+        response = self.callTelenet("https://api.prd.telenet.be/ocapi/oauth/userdetails","login", None, None)
+        if response.status_code == 200:
             # Return if already authenticated
             return
-        
         assert response.status_code == 401
+        # Fetch state & nonce
         state, nonce = response.text.split(",", maxsplit=2)
-
         # Log in
-        response = self.s.get(f'https://login.prd.telenet.be/openid/oauth/authorize?client_id=ocapi&response_type=code&claims={{"id_token":{{"http://telenet.be/claims/roles":null,"http://telenet.be/claims/licenses":null}}}}&lang=nl&state={state}&nonce={nonce}&prompt=login',timeout=10)
-            #no action
-        _LOGGER.debug("login result status code: " + str(response.status_code))
-        
-        response = self.s.post("https://login.prd.telenet.be/openid/login.do",data={"j_username": username,"j_password": password,"rememberme": True,},timeout=10)
-        _LOGGER.debug("login post result status code: " + str(response.status_code))
-        assert response.status_code == 200
-
+        self.callTelenet(f'https://login.prd.telenet.be/openid/oauth/authorize?client_id=ocapi&response_type=code&claims={{"id_token":{{"http://telenet.be/claims/roles":null,"http://telenet.be/claims/licenses":null}}}}&lang=nl&state={state}&nonce={nonce}&prompt=login',"login", None, None)
+        self.callTelenet("https://login.prd.telenet.be/openid/login.do","login",{"j_username": username,"j_password": password,"rememberme": True}, 200)
         self.s.headers["X-TOKEN-XSRF"] = self.s.cookies.get("TOKEN-XSRF")
-
-        response = self.s.get(
-            "https://api.prd.telenet.be/ocapi/oauth/userdetails",
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-            timeout=10,
-        )
-        _LOGGER.debug("get userdetails result status code: " + str(response.status_code))
-        assert response.status_code == 200
+        self.callTelenet("https://api.prd.telenet.be/ocapi/oauth/userdetails","login", None, 200)
 
     def userdetails(self):
-        response = self.s.get(
-            "https://api.prd.telenet.be/ocapi/oauth/userdetails",
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-        )
-        assert response.status_code == 200
+        response = self.callTelenet("https://api.prd.telenet.be/ocapi/oauth/userdetails","userdetails", None, 200)
         return response.json()
 
     def telemeter(self):
-        response = self.s.get(
-            "https://api.prd.telenet.be/ocapi/public/?p=internetusage,internetusagereminder",
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-            timeout=10,
-        )
-        _LOGGER.debug("telemeter result status code: " + str(response.status_code))
-        _LOGGER.debug("telemeter result " + response.text)
-        if response.status_code == 200:
-            # return next(Telemeter.from_json(response.json()))
-            return response.json()
+        response = self.callTelenet("https://api.prd.telenet.be/ocapi/public/?p=internetusage,internetusagereminder","telemeter", None, 200)
+        return response.json()
 
     def telemeter_product_details(self, url):
-        response = self.s.get(
-            url,
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-        )
-        assert response.status_code == 200
-        # _LOGGER.info("telemeter_product_details result " + response.text)
-        # json_string = response.text.replace("'",'"').replace("True","true").replace("False","false")
-        # _LOGGER.info("telemeter_product_details json_string " + json_string)
-        # return json.loads(json_string)
+        response = self.callTelenet(url,"telemeter_product_details",None, 200)
         return response.json()
         
     def mobile(self):
-        response = self.s.get(
-            "https://api.prd.telenet.be/ocapi/public/?p=mobileusage",
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-            timeout=10,
-        )
-        _LOGGER.info("mobile result status code: " + str(response.status_code))
-        _LOGGER.info("mobile result " + response.text)
-        if response.status_code == 200:
-            return response.json()
+        response = self.callTelenet("https://api.prd.telenet.be/ocapi/public/?p=mobileusage","mobile", None, 200)
+        return response.json()
         
     def planInfo(self):
-        response = self.s.get(
-            "https://api.prd.telenet.be/ocapi/public/api/product-service/v1/product-subscriptions?producttypes=PLAN",
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-            timeout=10,
-        )
-        _LOGGER.debug("planInfo result status code: " + str(response.status_code))
-        _LOGGER.debug("planInfo result " + response.text)
-        assert response.status_code == 200
-        # return next(Telemeter.from_json(response.json()))
+        response = self.callTelenet("https://api.prd.telenet.be/ocapi/public/api/product-service/v1/product-subscriptions?producttypes=PLAN","planInfo", None, 200)
         return response.json()
     
     def billCycles(self, productType, productIdentifier):
-        response = self.s.get(
-            f"https://api.prd.telenet.be/ocapi/public/api/billing-service/v1/account/products/{productIdentifier}/billcycle-details?producttype={productType}&count=3",
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-            timeout=10,
-        )
-        _LOGGER.debug("billCycles url: " + str(response.url))
-        _LOGGER.debug("billCycles result status code: " + str(response.status_code))
-        _LOGGER.debug("billCycles result " + response.text)
-        assert response.status_code == 200
-        # return next(Telemeter.from_json(response.json()))
+        response = self.callTelenet(f"https://api.prd.telenet.be/ocapi/public/api/billing-service/v1/account/products/{productIdentifier}/billcycle-details?producttype={productType}&count=3","billCycles", None, 200)
         return response.json()
     
     def productUsage(self, productType, productIdentifier,startDate, endDate):
-        response = self.s.get(
-            f"https://api.prd.telenet.be/ocapi/public/api/product-service/v1/products/{productType}/{productIdentifier}/usage?fromDate={startDate}&toDate={endDate}",
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-            timeout=10,
-        )
-        _LOGGER.debug("productUsage result status code: " + str(response.status_code))
-        _LOGGER.debug("productUsage result " + response.text)
-        assert response.status_code == 200
-        # return next(Telemeter.from_json(response.json()))
+        response = self.callTelenet(f"https://api.prd.telenet.be/ocapi/public/api/product-service/v1/products/{productType}/{productIdentifier}/usage?fromDate={startDate}&toDate={endDate}","productUsage", None, 200)
         return response.json()
 
     def productSubscriptions(self, productType):
-        response = self.s.get(
-            f"https://api.prd.telenet.be/ocapi/public/api/product-service/v1/product-subscriptions?producttypes={productType}",
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-            timeout=10,
-        )
-        _LOGGER.debug("productSubscriptions result status code: " + str(response.status_code))
-        _LOGGER.debug("productSubscriptions result " + response.text)
-        assert response.status_code == 200
+        response = self.callTelenet(f"https://api.prd.telenet.be/ocapi/public/api/product-service/v1/product-subscriptions?producttypes={productType}","productSubscriptions", None, 200)
         return response.json()
 
     def mobileUsage(self, productIdentifier):
-        response = self.s.get(
-            f"https://api.prd.telenet.be/ocapi/public/api/mobile-service/v3/mobilesubscriptions/{productIdentifier}/usages",
-            headers={
-                "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-            },
-            timeout=10,
-        )
-        _LOGGER.debug("mobileUsage result status code: " + str(response.status_code))
-        _LOGGER.debug("mobileUsage result " + response.text)
-        assert response.status_code == 200
+        response = self.callTelenet(f"https://api.prd.telenet.be/ocapi/public/api/mobile-service/v3/mobilesubscriptions/{productIdentifier}/usages","mobileUsage", None, 200)
         return response.json()
 
     def mobileBundleUsage(self, bundleIdentifier, lineIdentifier = None):
         if lineIdentifier != None:
-            response = self.s.get(
-                f"https://api.prd.telenet.be/ocapi/public/api/mobile-service/v3/mobilesubscriptions/{bundleIdentifier}/usages?type=bundle&lineIdentifier={lineIdentifier}",
-                headers={
-                    "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-                },
-                timeout=10,
-            )
+            response = self.callTelenet(f"https://api.prd.telenet.be/ocapi/public/api/mobile-service/v3/mobilesubscriptions/{bundleIdentifier}/usages?type=bundle&lineIdentifier={lineIdentifier}","mobileBundleUsage lineIdentifier", None, 200)
         else:
-            response = self.s.get(
-                f"https://api.prd.telenet.be/ocapi/public/api/mobile-service/v3/mobilesubscriptions/{bundleIdentifier}/usages?type=bundle",
-                headers={
-                    "x-alt-referer": "https://www2.telenet.be/nl/klantenservice/#/pages=1/menu=selfservice",
-                },
-                timeout=10,
-            )
-        _LOGGER.debug("mobileBundleUsage result status code: " + str(response.status_code))
-        _LOGGER.debug("mobileBundleUsage result " + response.text)
-        assert response.status_code == 200
+            response = self.callTelenet(f"https://api.prd.telenet.be/ocapi/public/api/mobile-service/v3/mobilesubscriptions/{bundleIdentifier}/usages?type=bundle","mobileBundleUsage bundle", None, 200)
         return response.json()
