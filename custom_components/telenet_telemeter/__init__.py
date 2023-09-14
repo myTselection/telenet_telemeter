6832912db08b8c291b52e8999577211328a3c63d
myTselection <myTselection@gmail.com>
2023-09-14 23:16:29 +0200

    services defined, but testing fails because of telenet maintenance

--- custom_components/telenet_telemeter/__init__.py
+++ custom_components/telenet_telemeter/__init__.py
@@ -6,7 +6,7 @@ from homeassistant import config_entries
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Config, HomeAssistant
 from homeassistant.const import Platform
-from .utils import *
+from .utils import TelenetSession
 from homeassistant.const import (
     CONF_NAME,
     CONF_PASSWORD,
@@ -81,6 +81,8 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(config_entry, Platform.SENSOR)
     )
+    _LOGGER.info(f"{DOMAIN} register_services")
+    register_services(hass, config_entry)
     return True
 
 
@@ -90,3 +92,63 @@ async def async_remove_entry(hass, config_entry):
         _LOGGER.info("Successfully removed sensor from the integration")
     except ValueError:
         pass
+
+
+def register_services(hass, config_entry):
+    
+    async def handle_switch_wireless(enableWifi, enableWifree):
+        """Handle the service call."""
+        
+        config = config_entry.data
+        username = config.get("username")
+        password = config.get("password")
+        internet = config.get("internet")
+        if not internet:
+            return
+        session = TelenetSession()
+        await hass.async_add_executor_job(lambda: session.login(username, password))
+        v2 = await hass.async_add_executor_job(lambda: session.apiVersion2())
+        if not v2:
+            return
+        
+        customerDetails = await hass.async_add_executor_job(lambda: session.customerdetails())
+        customerLocationId = customerDetails.get('customerLocations')[0].get('id')
+        
+        internetProductDetails = await hass.async_add_executor_job(lambda: session.productSubscriptions("INTERNET"))
+        internetProductIdentifier = internetProductDetails[0].get('identifier')
+
+        modemDetails = await hass.async_add_executor_job(lambda: session.modemdetails(internetProductIdentifier))
+        modemMac = modemDetails.get('mac')
+
+        wifiDetails = await hass.async_add_executor_job(lambda: session.wifidetails(internetProductIdentifier, modemMac))
+        wifiEnabled = wifiDetails.get('wirelessEnabled')
+        wifreeEnabled = wifiDetails.get('homeSpotEnabled')
+
+        if enableWifi is None:
+            enableWifi = wifiEnabled
+        if enableWifree is None:
+            enableWifree = wifreeEnabled
+    
+        response  = await hass.async_add_executor_job(lambda: session.switchWifi(enableWifree, enableWifi, internetProductIdentifier, modemMac, customerLocationId))
+
+        _LOGGER.debug(f"{NAME} handle_switch_wifi switch executed, old state: wifiEnabled: {wifiEnabled}, wifreeEnabled: {wifreeEnabled}, new state: enableWifi: {enableWifi}, enableWifree: {enableWifree}, response: {response}")
+        
+    async def handle_switch_wifi(call):
+        """Handle the service call."""
+        
+        enable = call.data.get('enable')
+        if not(enable in ["Yes","No"]):
+            return
+        await handle_switch_wireless(enable, None)
+        
+    async def handle_switch_wifree(call):
+        """Handle the service call."""
+        enable = call.data.get('enable')
+        if not(enable in ["Yes","No"]):
+            return
+        await handle_switch_wireless(None, enable)
+
+
+    hass.services.async_register(DOMAIN, 'switch_wifi', handle_switch_wifi)
+    hass.services.async_register(DOMAIN, 'switch_wifree', handle_switch_wifree)
+    _LOGGER.info(f"async_register done")
